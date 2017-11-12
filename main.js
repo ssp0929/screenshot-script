ded116f0458f99e080c88345843fbfe07c24ce05
Stephen Park <ssp0929@gmail.com>
2017-11-12 12:12:24 -0600

    After inputting URL and name of saved file, can run headless chrome to go to that url and take a screenshot.

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+
+const pathToThisDirectory = path.dirname(fs.realpathSync(__filename));
+
+const url = 'http://stephensp.com';
+const name = 'stephensp.png';
 
 async function getPic() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://google.com');
-  await page.screenshot({path: 'google.png'});
 
+  await page.goto(url);
+  await page.screenshot({path: path.join(pathToThisDirectory, '/images/', name) });
   await browser.close();
 }
 
